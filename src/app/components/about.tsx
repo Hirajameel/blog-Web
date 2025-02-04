9a430e1b923deee12670bfaf169c17708d1dd0d5
Hirajameel <160866608+Hirajameel@users.noreply.github.com>
2025-02-04 05:05:16 -0800

    changes

--- src/app/components/about.tsx
+++ src/app/components/about.tsx
@@ -10,7 +10,8 @@ export default function About() {
           <p className="text-lg leading-relaxed tracking-wide text-white animate-slide-in">
             Welcome to my blog! My name is <span className="font-bold text-yellow-300">Hira</span>, and I am passionate about exploring the wonders of the world, whether it's the beauty of nature, the richness of wildlife, or the mysteries of the deep sea. Through this platform, I aim to share my experiences, thoughts, and insights with readers who share a love for discovery and inspiration.
             <br /><br />
-            I believe in the power of storytelling to connect people with the natural world. Each post is crafted with care to inspire awe and respect for the environment and its incredible diversity. When I'm not writing, you can find me hiking in the mountains, photographing wildlife, or diving into new adventures.
+            I believe in the power of storytelling to connect people with the natural world. Each post is crafted with care to inspire awe and respect for the environment and its incredible diversity.When I&apos;m not writing, you can find me hiking in the mountains, photographing wildlife, or diving into new adventures.
+
             <br /><br />
             Thank you for visiting my blog. I hope you enjoy the journey as much as I do!
           </p>
